5621c4d987b239eb52baeda2692bcd23a352b31c
Christopher <windikite@yahoo.com>
2024-02-07 21:45:26 -0500

    v0.0.6 - visual changes, more info displayed

--- src/components/CharacterBuilder.js
+++ src/components/CharacterBuilder.js
@@ -13,8 +13,47 @@ function CharacterBuilder({
         characterData, 
         setCharacterData,
     }) {
-    const [classData, setClassData] = useState();
-    const [raceData, setRaceData] = useState();
+    const dummy = {
+        _id: "",
+        name: "",
+        helpText: "",
+        hitDice: "",
+        levelMod: 0,
+        size: "",
+        speed: 0,
+        source: [
+            {
+                book: "",
+                page: null
+            }
+        ],
+        saves: {
+            fort: "",
+            will: "",
+            ref: ""
+        },
+        skillPointModifier: 0,
+        skills: "",
+        statBonuses: {
+            con: 0,
+            str: 0,
+            dex: 0,
+            wis: 0,
+            int: 0,
+            cha: 0
+        },
+        features: {
+            lv1: [
+                {
+                    name: "",
+                    text: ""
+                }
+            ]
+        }
+    };
+
+    const [classData, setClassData] = useState(dummy);
+    const [raceData, setRaceData] = useState(dummy);
 
     function checkStage(stage){
         if(stage === 0){
@@ -28,14 +67,14 @@ function CharacterBuilder({
                     <List 
                         list={data[0]}
                         name="Class"
-                        h1="Level Mod"
+                        h1="Features"
                         h2="Help Text"
                         h3="Saves"  
                         h4="Skills"
                         h5="Source"
-                        c1="levelMod"
+                        c1="features[lv1]"
                         c2="helpText"
-                        c3={["fort", "will", "ref"]}
+                        c3="saves"
                         c4="skills"
                         c5="source"
                         setSelectedData={setClassData}
@@ -49,12 +88,12 @@ function CharacterBuilder({
                     <List 
                         list={data[1]}
                         name="Race"
-                        h1="Level Mod"
+                        h1="Features"
                         h2="Help Text"
                         h3="Stat Bonuses"  
                         // h4="Skills"
                         h5="Source"
-                        c1="levelMod"
+                        c1="features[lv1]"
                         c2="helpText"
                         c3={["con", "str", "dex", "wis", "int", "cha"]}
                         // c4="skills"
@@ -70,12 +109,12 @@ function CharacterBuilder({
         <Container>
             <ProgressBar stage={stage} />
             <NameInput />
-            <CharacterSheet 
+            {/* <CharacterSheet 
                 // raceData={data[1]} 
                 // classData={data[0]} 
                 selectedRace={raceData} 
                 selectedClass={classData}
-                />
+                /> */}
             <Container className='mt-3 mb-3 p-3 bg-dark-subtle border border-dark-subtle rounded-3'>
                 {checkStage(stage)}
             </Container>
--- src/components/Item.js
+++ src/components/Item.js
@@ -8,20 +8,48 @@ function Item({
     c5,
     setSelectedData
 }) {
+    // function checkVariable(c){
+    //     if(typeof c === 'string' || c instanceof String){
+    //         return <td>{item[c]}</td> 
+    //     }else if(typeof c === 'array' || c instanceof Array){
+    //         let spreadVar = [];
+    //         // function convert(i){
+    //         //     function findAndClean(x){
+    //         //         let foundIndex = x.indexOf(".");
+    //         //         let cleanedFirst = x.slice(0, foundIndex);
+    //         //         let cleanedLast = x.slice(foundIndex +1);
+    //         //         console.log(`vars`, x, foundIndex, cleanedFirst, cleanedLast)
+    //         //         return [cleanedFirst, cleanedLast]
+    //         //     }
+    //         //     let cleanedVar = findAndClean(i)
+    //         //     let data = item[cleanedVar[0]];
+    //         //     spreadVar.push(`${cleanedVar[1]}: ${data[cleanedVar[1]]}, `)
+    //         // }
+    //         let 
+    //         c.map(x => convert(x));
+    //         let fullvar = <td>{spreadVar}</td>
+    //         return fullvar;
+    //     }
+        
+    // }
     function checkVariable(c){
-        if(typeof c === 'string' || c instanceof String){
-            return <td>{item[c]}</td> 
-        }else if(typeof c === 'array' || c instanceof Array){
-            let spreadVar = [];
-            function convert(i){
-                spreadVar.push(`${i}: ${item[i]}, `)
+        let data = item[c];
+        if(typeof data === 'object' || data instanceof Object){
+            const output = [];
+            const keys = Object.keys(data);
+            const values = Object.values(data);
+            for (let i = 0; i < keys.length; i++) {
+                output.push(`${keys[i]}: ${values[i]} `)
             }
-            c.map(x => convert(x));
-            let fullvar = <td>{spreadVar}</td>
-            return fullvar;
+            console.log(`keys`, keys, `values`, values, `output`, output)
+            return <td>{output}</td>
+        }else if(typeof data === 'array' || data instanceof Array){
+
+        }else{
+            return <td>{data}</td>
         }
         
-    }
+    };
     const column1 = c1 ? checkVariable(c1) : null;
     const column2 = c2 ? checkVariable(c2) : null;
     const column3 = c3 ? checkVariable(c3) : null;
